7664bac0b344b4741bf6989dab30f6d93c7c735b
jcmd1994j <167235400+jcmd1994j@users.noreply.github.com>
2024-07-07 16:55:02 -0300

    Add files via upload

---
+++ exer01.js
@@ -0,0 +1,52 @@
+const questaA =4 + 6 * 2;
+const questaB = 10 / 5 + 8;
+const questaC =12 / 4 + 16 / 2;
+const questaD = 17 + 20/ 5;
+const questaE =22 / 5 + 7;
+const questaF =39 / 10;
+const questaG = 19 % (2 + 3);
+const questaH = 3 + 4 * 20 / 3;
+const questaI =36 % (6 + 2);
+const questaJ = 8 % 2 * 0
+
+
+console.log("Valor da questão a",questaA);
+console.log("Valor da questão a",questaB);
+console.log("Valor da questão a",questaC);
+console.log("Valor da questão a",questaD);
+console.log("Valor da questão a",questaE);
+console.log("Valor da questão a",questaF);
+console.log("Valor da questão a",questaG);
+console.log("Valor da questão a",questaH);
+console.log("Valor da questão a",questaI);
+console.log("Valor da questão a",questaJ);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*1. Expressões numéricas – Qual é o valor numérico de cada uma das seguintes
+expressões numéricas? Considere as regras de prioridade dos operadores
+aritméticos ao responder.
+Dica: o operador MOD (ou %) retorna o resto da divisão inteira. Por exemplo, 7
+MOD 2 = 7 % 2 = 1 (a divisão inteira de 7 por 2 é 3 e o resto dessa divisão é 1. O
+operador MOD retorna esse resto).
+a. 4 + 6 * 2
+b. 10 / 5 + 8
+c. 12 / 4 + 16 / 2
+d. 17 + 20/ 5
+e. 22 / 5 + 7
+f. 39 / 10
+g. 19 % (2 + 3)
+h. 3 + 4 * 20 / 3
+i. 36 % (6 + 2)
+j. 8 % 2 * 0
+*/
