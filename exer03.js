fe8b9ded2d65df8cff3715c3807e0f36f54776bf
jcmd1994j <167235400+jcmd1994j@users.noreply.github.com>
2025-11-02 09:38:12 -0300

    Update exer03.js

--- exer03.js
+++ exer03.js
@@ -1,23 +1,18 @@
-const readlineSync = require('readline-sync');
-const nome = readlineSync.question("Qual é seu nome?");
-const endereco = readlineSync.question("Qual é seu endereço?");
-const cidade = readlineSync.question("Qual é sua cidade?");
-const estado= readlineSync.question("Qual é se Estado ?");
-const cep = Number (readlineSync.question("Qual é seu Cep?"));
-const telefone =Number (readlineSync.question("Qual é seu telefone?"));
-const profissao  = readlineSync.question("Qual é sua profissão?");
+const readlineSync =require('readline-sync')
 
+const nome =readlineSync.question("Qual é seu nome ?");
+const endereco=readlineSync.question("Qual é seu endereço?");
+const cidade=readlineSync.question("Qual sua cidade ?");
+const estado=readlineSync.question("Qual seu estado ?");
+const telefone=readlineSync.question("Número de telefone ?");
+const profissao=readlineSync.question("Sua profissão ?");
 
-
-
-console.log("nome:",nome);
-console.log("Endereço:",endereco);
-console.log("Cidade:",cidade);
-console.log("Estado:",estado);
-console.log("Cep:",cep);
-console.log("Telefone:",telefone);
-console.log("Profissão:",profissao);
-console.log("Endereço Completo:",endereco+" "+cidade+" "+estado+" "+cep);
+console.log(`Nome:${nome}`)
+console.log(`Cidade:${cidade}`)
+console.log(`Estado:${estado}`)
+console.log(`Telefone:${telefone}`)
+console.log(`Profissão:${profissao}`)
+console.log(`Endereço:${endereco,+cidade+,+estado+}`)
 
 
 
@@ -32,11 +27,10 @@ console.log("Endereço Completo:",endereco+" "+cidade+" "+estado+" "+cep);
 
 
 
-
-/*3. Informações Pessoais – Crie um algoritmo que peça ao usuário para informar os
-dados abaixo e, depois disso, os imprima na tela:
-• Nome;
-• Endereço, com cidade, estado e CEP;
-• Número de telefone;
-• Área de atuação profissiona
+/*3. Informações Pessoais – Crie um algoritmo que peça ao usuário para informar os 
+dados abaixo e, depois disso, os imprima na tela: 
+• Nome; 
+• Endereço, com cidade, estado e CEP; 
+• Número de telefone; 
+• Área de atuação profissional.
 */
