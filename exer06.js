519c5960fe34f07177d592394e0ce5268488002d
jcmd1994j <167235400+jcmd1994j@users.noreply.github.com>
2025-11-02 10:23:23 -0300

    Update exer06.js

--- exer06.js
+++ exer06.js
@@ -1,20 +1,17 @@
-const readlineSync = require('readline-sync');
-const macarrao= Number (readlineSync.question("Qual foi o valor do macarrão ?"));
-const arroz= Number (readlineSync.question("Qual foi o valor arroz ?"));
-const feijao= Number (readlineSync.question("Qual foi o valor do feijão ?"));
-const peixe= Number (readlineSync.question("Qual foi o valor do peixe?"));
-const carne= Number (readlineSync.question("Qual foi o valor da carne?"));
+const readlineSync=require('readline-sync');
+
+const tenis=Number(readlineSync.question("Qual valor de tênis ?"));
+const bolsa=Number(readlineSync.question("Valor da bolsa ?"));
+const saia=Number(readlineSync.question("Qual valor da saia ?"));
+const calca=Number(readlineSync.question("Valor da calça?"));
+const blusa=Number(readlineSync.question("Qual valor da bolsa ?"));
 
 
-soma=macarrao+arroz+feijao+peixe+carne;
-imposto=soma*6/100;
+subtotal=tenis+blusa+saia+calca+blusa;
+imposto=subtotal*6/100;
 
-
-console.log("Valor da venda :",soma,"R$")
-console.log("Valor da porcentagem:",imposto,"%")
-console.log("Valor total da venda :",soma+imposto,"R$")
-
-
+console.log(`Valor do subtotal das compras : ${subtotal} R$`);
+console.log(`Valor dos impostos : ${imposto} R$`);
 
 
 
@@ -27,10 +24,9 @@ console.log("Valor total da venda :",soma+imposto,"R$")
 
 
 
-
-/*6. Valores de uma Compra – Um cliente de uma loja está comprando cinco
-produtos. Crie um algoritmo que solicite o preço de cada um desses produtos e,
-em seguida, exiba o subtotal da venda, o valor do imposto e o valor total
-(subtotal da venda mais o valor do imposto). Suponha que a alíquota do imposto
+/*6. Valores de uma Compra – Um cliente de uma loja está comprando cinco 
+produtos. Crie um algoritmo que solicite o preço de cada um desses produtos e, 
+em seguida, exiba o subtotal da venda, o valor do imposto e o valor total 
+(subtotal da venda mais o valor do imposto). Suponha que a alíquota do imposto 
 seja de 6% sobre o subtotal da venda.
 */
