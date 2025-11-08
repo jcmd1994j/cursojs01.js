39ad358133b89f3966b95c0dba5b8eaf92ada33d
jcmd1994j <167235400+jcmd1994j@users.noreply.github.com>
2025-11-08 14:14:22 -0300

    Fix variable names and update calorie calculation

--- exer13.js
+++ exer13.js
@@ -1,21 +1,25 @@
-const readlineSync = require('readline-sync');
-const biscoito= Number (readlineSync.question("Quantos pacotes de biscoitos você vai comprar?"));
-
-
-calorias=biscoito*300;
-
-console.log("calorias que você vai ter:",calorias,"calorias")
+const readlineSny=require('readline-sync')
+const biscoitos=Number(readlineSny.question("Quantos biscoitos forao consumidos?"));
 
 
 
+calorias=biscoitos*300;
+
+
+console.log(`bistoitos consumidos: ${biscoitos}`)
+
+console.log(`Calorias consumidas : ${calorias}`)
 
 
 
 
 
-/*13. Calorias dos Biscoitos – Um saco de biscoitos contém 40 unidades que, de
-acordo com as informações do rótulo, equivalem a 10 porções. Ainda de acordo
-com o rótulo, uma porção possui 300 calorias. Baseado nessas informações, crie
-um algoritmo que permita ao usuário inserir o número de biscoitos que ele
+
+
+
+/*13. Calorias dos Biscoitos – Um saco de biscoitos contém 40 unidades que, de 
+acordo com as informações do rótulo, equivalem a 10 porções. Ainda de acordo 
+com o rótulo, uma porção possui 300 calorias. Baseado nessas informações, crie 
+um algoritmo que permita ao usuário inserir o número de biscoitos que ele 
 consumiu e imprima na tela a quantidade de calorias correspondentes.
 */
