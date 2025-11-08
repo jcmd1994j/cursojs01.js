a0e9946cd4f7a871d245d8b9facb95f7d7c39006
jcmd1994j <167235400+jcmd1994j@users.noreply.github.com>
2025-11-08 17:22:12 -0300

    Refactor voting percentage calculation for parties

--- exer23.js
+++ exer23.js
@@ -1,25 +1,36 @@
-const readlineSync = require('readline-sync');
-const partido1= Number (readlineSync.question("Quantos votos para o partido 1?"));
-const partido2= Number (readlineSync.question("Quantos votos para o partido 2?"));
-const partido3= Number (readlineSync.question("Quantos votos para o partido 3?"));
-
-porPart1=partido1*100/100;
-porPart2=partido2*100/100;
-porPart3=partido3*100/100;
-
-console.log("valor da porcentagem de partido1:",porPart1,"%")
-console.log("valor da porcentagem de partido2:",porPart2,"%")
-console.log("valor da porcentagem de partido3:",porPart3,"%")
+const readlineSny=require('readline-sync')
+const partidoVerde=Number(readlineSny.question("Quantos votos o Partido Verde tem ?"));
+const partidoVermelho=Number(readlineSny.question("Quantos votos o Partido Vermelho tem?"));
+const partidoAmarelo=Number(readlineSny.question("Quantos votos o Partido Amarelo tem ?"));
 
 
+calculoPartidoVerde=partidoVerde*1/100;
+calculoPartidoVermelho=partidoVermelho*1/100;
+calculoPartidoAmarelo=partidoAmarelo*1/100;
 
 
+console.log(`Porcentagem dos votos do Partido Verde : ${calculoPartidoVerde} %`);
+console.log(`Porcentagem dos votos do Partido Vermelho : ${calculoPartidoVermelho} %`);
+console.log(`Porcentagem dos votos do Partido Amarelo : ${calculoPartidoAmarelo} %`);
 
 
 
 
 
-/*23. Percentual de votos – Escreva um algoritmo que aceite os nomes de três
-partidos políticos e o número de votos que cada um recebeu na última eleição
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*23. Percentual de votos – Escreva um algoritmo que aceite os nomes de três 
+partidos políticos e o número de votos que cada um recebeu na última eleição 
 para prefeito. Exibir a porcentagem de votos que cada partido recebeu.
 */
