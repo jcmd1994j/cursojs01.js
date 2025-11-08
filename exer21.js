03039d191538d0853d5dd04edd7b3587726a9239
jcmd1994j <167235400+jcmd1994j@users.noreply.github.com>
2025-11-08 16:42:58 -0300

    Fix variable names and improve output formatting

--- exer21.js
+++ exer21.js
@@ -1,23 +1,20 @@
-const readlineSync = require('readline-sync');
-const moedas= Number (readlineSync.question("Qual valor você precisa troca ?"));
-//1.00, R$ 0.50, R$ 0.25, R$0.10, R$ 0.05 e R$ 0.01.
+const readlineSny=require('readline-sync')
+const moedinhas=Number(readlineSny.question("Qual valor você precisa troca ?"));
+//1,00 - 0,50 -0.25 -0.10-0,05-0.01
 
+umReal=moedinhas*100/100;
+cinquenta=moedinhas*100/50;
+vinteCinco=moedinhas*100/25;
+dezCent=moedinhas*100/10;
+cincoCent=moedinhas*100/5;
+umCent=moedinhas*100/1;
 
-umReal=moedas*100/100
-cinquent=moedas*100/5
-vintecinco=moedas*100/25
-dez=moedas*100/10
-centcinco=moedas*100/5
-umcent=moedas*100/1
-
-console.log("Troco de 1 real:",umReal,"moedas");
-console.log("Troco de 0,50 centavos:",cinquent,"moedas");
-console.log("Troco de 0,25 centavos:",vintecinco,"moedas");
-console.log("Troco de 0,10 centavos:",dez,"moedas");
-console.log("Troco de 0,05 centavos:",centcinco,"moedas");
-console.log("Troco de 0,01 centavos:",umcent,"moedas");
-
-
+console.log(`Troco de 1 real:${umReal} moedas`);
+console.log(`Troco de 50 centavos:${cinquenta} moedas`);
+console.log(`Troco de 25 centavos:${vinteCinco} moedas`);
+console.log(`Troco de 10 centavos:${dezCent} moedas`);
+console.log(`Troco de 5 centavos:${cincoCent} moedas`);
+console.log(`Troco de 1 centavos:${umCent} moedas`);
 
 
 
@@ -35,9 +32,13 @@ console.log("Troco de 0,01 centavos:",umcent,"moedas");
 
 
 
-/*21. Troco em moedas – Crie um algoritmo que calcule e exiba a conversão de uma
-determinada quantidade em reais em moedas de R$ 1.00, R$ 0.50, R$ 0.25, R$
-0.10, R$ 0.05 e R$ 0.01. Por exemplo, R$ 3.78 resulta em três moedas de um real,
-uma de cinquenta centavos, duas de dez centavos, uma de 5 centavos e três de
-um centavo.
+
+
+
+
+/*21. Troco em moedas – Crie um algoritmo que calcule e exiba a conversão de uma 
+determinada quantidade em reais em moedas de R$ 1.00, R$ 0.50, R$ 0.25, R$ 
+0.10, R$ 0.05 e R$ 0.01. Por exemplo, R$ 3.78 resulta em três moedas de um real, 
+uma de cinquenta centavos, duas de dez centavos, uma de 5 centavos e três de 
+um centavo.  
 */
