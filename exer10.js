a97bf8c84c7277f532fb7731c4134b0a3bfe74af
jcmd1994j <167235400+jcmd1994j@users.noreply.github.com>
2025-11-08 13:02:04 -0300

    Refactor meal cost calculation and fix variable names

--- exer10.js
+++ exer10.js
@@ -1,19 +1,16 @@
-const readlineSync = require('readline-sync');
-const adulto= Number (readlineSync.question("Quantas marmitas para adultos ?"));
-const crianca= Number (readlineSync.question("Quantas marmitas para crianças ?"));
+const readlineSny=require('readline-sync');
+const refeicao=Number(readlineSny.question("Qual valor da refeição?"));
 
 
-gorgeta=adulto+crianca*15/100;
-imposto=adulto+crianca*7/100;
-total=gorgeta+imposto;
+gorjeta=refeicao*15/100;
+imposto=refeicao*7/100;
+valorTotal=gorjeta+imposto+refeicao;
 
 
-
-
-console.log("Valor da gorjeta :",gorgeta,"R$")
-console.log("Valor da porcentagem:",imposto,"%")
-console.log("Valor total da venda :",total,"R$")
-
+console.log(`Valor da refeição: ${refeicao} R$`);
+console.log(`Valor da gorjeta: ${gorjeta} R$`);
+console.log(`Valor do imposto: ${imposto} R$`);
+console.log(`Valor da total: ${valorTotal} R$`);
 
 
 
@@ -24,10 +21,26 @@ console.log("Valor total da venda :",total,"R$")
 
 
 
-/*10. Gorjeta, Imposto e Total Pago por uma Refeição – Crie um algoritmo que peça
-para o usuário informar o valor de uma refeição num restaurante. A partir desse
-valor, o algoritmo deverá calcular o valor da gorjeta (15% sobre o valor da
-refeição), dos impostos (7% do valor da refeição) e o total a ser pago (valor da
-refeição mais os valores da gorjeta e dos impostos). Ao final, o algoritmo deverá
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*10. Gorjeta, Imposto e Total Pago por uma Refeição – Crie um algoritmo que peça 
+para o usuário informar o valor de uma refeição num restaurante. A partir desse 
+valor, o algoritmo deverá calcular o valor da gorjeta (15% sobre o valor da 
+refeição), dos impostos (7% do valor da refeição) e o total a ser pago (valor da 
+refeição mais os valores da gorjeta e dos impostos). Ao final, o algoritmo deverá 
 exibir cada um desses valores e o total.
 */
