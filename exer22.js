4bf777c05644bab0eb407dbeb507828e99212931
jcmd1994j <167235400+jcmd1994j@users.noreply.github.com>
2025-11-08 17:06:35 -0300

    Update exer22.js

--- exer22.js
+++ exer22.js
@@ -1,20 +1,23 @@
-
-const readlineSync = require('readline-sync');
-
-const minutos = Number(readlineSync.question("Digite a quantidade de minutos: "));
-
-const horas = minutos / 60;
-const dias = horas / 24;
+const readlineSny=require('readline-sync')
+const minutos=Number(readlineSny.question("Quantas minutos são?"));
+const hora =Number(readlineSny.question("Que horas são?"));
 
 
-console.log(minutos, "minutos equivalem a ",horas," horas");
-console.log(minutos, "minutos equivalem a ",dias,"dias");
+MinutoHoras=minutos/60;
+horaDias=hora/24;
 
 
 
+console.log(`${minutos}minutos equivalem a ${hora} horas e é igual a ${horaDias} dias`)
 
 
-/*22. Conversão de unidades (minutos para horas e dias) - Escreva um programa que
-aceite uma quantidade de minutos e o converta em horas e dias. Por exemplo,
+
+
+
+
+
+
+/*22. Conversão de unidades (minutos para horas e dias) - Escreva um programa que 
+aceite uma quantidade de minutos e o converta em horas e dias. Por exemplo, 
 6.000 minutos equivalem a 100 horas e é igual a 4.167 dias.
 */
