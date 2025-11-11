f414ccd9e00e91add867b1d9a3ee722e771e998f
jcmd1994j <167235400+jcmd1994j@users.noreply.github.com>
2025-11-11 08:23:59 -0300

    Add boolean checks and new variable initializations
    Refactor the code to include new variables and boolean checks.

--- cond05.js
+++ cond05.js
@@ -1,30 +1,68 @@
-const readlineSync= require ("readline-sync");
-const livros=Number (readlineSync.question("Quantos livros o cliente comprou?"));
+const readlineSny=require('readline-sync')
+numero1 = 300
+numero2 = 100 
+numero3 = 5
+string1 = "Rinoceronte"
+string2 ="Zebra"
+string3 ='bug'
 
 
-const livro0=0;
-const livro1=5;
-const livro2=15;
-const livro3=30;
-const livro4=60;
-
-if(livros == 0){
-    console.log("Você ganhou:",livro0,"pontos")
+if(numero1 == numero3){
+console.log('Falso')
 }
-else if(livros == 1){
-    console.log("Você ganhou:",livro1,"pontos")
+if(numero1 > numero3){
+console.log('Verdadeiro')
 }
-else if(livros == 2){
-    console.log("Você ganhou:",livro2,"pontos")
+if(numero2 < numero3){
+console.log('Falso')
 }
-else if(livros == 3){
-    console.log("Você ganhou:",livro3,"pontos")
+if(numero1 == string1){
+console.log('ilegal')
 }
-else if(livros >= 4){
-    console.log("Você ganhou:",livro4,"pontos")
+if(numero1 == 'Um'){
+console.log('ilegal')
 }
-else{
-    console.log("erro")
+if(numero1 == 'Trezentos'){
+console.log('Falso')
+}
+if(numero1 == '300'){
+console.log('Falso')
+}
+if(string2 == 'Dois'){
+console.log('Falso')
+}
+if(string1 == 'Rinoceronte'){
+console.log('Verdadeiro')
+}
+if(string2 != 'Rinoceronte'){
+console.log('Verdadeiro')
+}
+if(string2 != 'Rinoceronte'|| numero1 > numero3){
+console.log('Verdadeiro')
+}
+if(numero2 <= numero1 / 3){
+console.log('Verdadeiro')
+}
+if(numero1 >= 200 ){
+console.log('Verdadeiro')
+} 
+if(numero1 >= numero2 + numero3){
+console.log('Verdadeiro')
+}
+if(numero1 > numero2 && numero1 < numero3 ){
+console.log('Verdadeiro')
+}
+if(numero1 <= 100 || numero1 > numero3 ){
+console.log('falso')
+}
+if(numero1 < 10 || numero3 > 10  ){
+console.log('falso')
+}
+if(numero1 == 30  && numero2 == 100 || numero3 == 100){
+console.log('falso')
+}
+else {
+console.log(`erro`)
 }
 
 
@@ -44,16 +82,27 @@ else{
 
 
 
-
-
-
-/*5. O programa de fidelidade de uma determinada livraria premia seus clientes de
-acordo com o número de livros comprados a cada mês. Os pontos são atribuídos
-da seguinte forma:
-• Se um cliente comprar 0 livros, ele ganhará 0 pontos.
-• Se um cliente comprar um livro, ele ganhará 5 pontos.
-• Se um cliente comprar dois livros, ele ganhará 15 pontos.
-• Se um cliente comprar 3 livros, ele ganhará 30 pontos.
-• Se um cliente comprar 4 ou mais livros, ele ganhará 60 pontos.
-Crie um algoritmo que leia o número de livros comprado por um usuário e exiba
-o número de pontos correspondentes. */
+/*1. Considere as variáveis abaixo, inicializadas como segue:
+numero1 = 300 numero2 = 100 numero3 = 5
+string1 = "Rinoceronte" string2 = "Zebra" string3 = "bug”
+Para cada uma das seguintes expressões booleanas, classifique-a como
+Verdadeira, Falsa ou Ilegal.
+a. numero1 = numero3
+b. numero1 > numero3
+c. numero2 < numero3
+d. numero1 = string1
+e. numero1 = "Um"
+f. numero1 = "Trezentos"
+g. numero1 = "300"
+h. string2 = "Dois"
+i. string1 = "Rinoceronte"
+j. string3 <> "Rinoceronte"
+k. string3 <> "Rinoceronte" OU numero1 > 1000
+l. numero2 <= numero1 / 3
+m. numero1 >= 200
+n. numero1 >= numero2 + numero3
+o. numero1 > numero2 E numero1 < numero3
+p. numero1 = 100 OU numero1 > numero3
+q. numero1 < 10 OU numero3 > 10
+r. numero1 = 30 E numero2 = 100 OU numero3 = 100
+*/
