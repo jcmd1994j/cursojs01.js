d18708b9d195e1243d6712951c8f4cd643a065fb
jcmd1994j <167235400+jcmd1994j@users.noreply.github.com>
2025-11-11 07:07:59 -0300

    Fix logic and formatting in cond03.js

--- cond03.js
+++ cond03.js
@@ -1,21 +1,19 @@
-const readlineSync=require('readline-sync');
+const readlineSny=require('readline-sync')
+const produto=Number(readlineSny.question('Qual valor do produto ?'))
 
-const produto=Number(readlineSync.question("Qual o valor do produto?"))
 
-calculo45=produto*45/100;
 calculo30=produto*30/100;
+calculo45=produto*45/100;
 
-
-if (50 > produto){
-console.log(`O produto será vendido por 45% porque foi abaixo de 50,00 R$ ou seja :${calculo45} R$`)
+if(50 > produto ){
+    console.log(`Vendido por 45% porque foi MENOR que 50 reias ${calculo45} R$`);
 }
-else if( 50 < produto ){
-    console.log(`O produto será vendido por 30% de lucro se for um valor de 50,00 R$ ou mais ou seja :${calculo30} R$`)
+else if (50 < produto ){
+    console.log(`Vendido por 30 % porque foi MAIOR  que 50 reias ${calculo30} R$`);
 }
 
 
 
-//////
 
 
 
@@ -27,14 +25,10 @@ else if( 50 < produto ){
 
 
 
-/////
-/// > maior
-///< menor 
-/////
 
-
-/* 3. Um brechó revende produtos usados, e fixa o preço de venda de cada produto 
+/*3. Um brechó revende produtos usados, e fixa o preço de venda de cada produto 
 conforme o valor de sua aquisição: se o preço de aquisição de um produto é menor 
 do de R$ 50.00, ele deve ser vendido por um preço 45% maior; caso contrário, o 
 lucro será de 30%. Sabendo disso, construa um algoritmo que leia o valor de 
-aquisição de um produto e mostre o seu valor de venda. */
+aquisição de um produto e mostre o seu valor de venda. 
+*/
