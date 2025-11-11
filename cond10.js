f0995b2b514f2015f6c49113706ca98e97b23077
jcmd1994j <167235400+jcmd1994j@users.noreply.github.com>
2025-11-11 11:42:17 -0300

    Refactor discount calculation and output logic

--- cond10.js
+++ cond10.js
@@ -1,59 +1,45 @@
 const readlineSync = require("readline-sync");
-const jogos = Number(readlineSync.question("Quantos jogos você comprou? "));
+const jogos= Number(readlineSync.question("Quantos jogos você comprou  ? "));
+desconto20=150-0.20;
+desconto30=150-0.30;
+desconto40=150-0.40;
+desconto50=150-0.30;
 
-const precoPorJogo = 150;
-let desconto = 0;
 
-if (jogos >= 10 && jogos <= 19) {
-    desconto = 0.20;
-} else if (jogos >= 20 && jogos <= 49) {
-    desconto = 0.30;
-} else if (jogos >= 50 && jogos <= 99) {
-    desconto = 0.40;
-} else if (jogos >= 100) {
-    desconto = 0.50;
-} else if (jogos < 10) {
-    desconto = 0;
-} else {
-    console.log("Erro");
-    
+
+
+
+if(jogos >= 10 && jogos >= 19){
+console.log(`Valor a pagar  : ${desconto20}`)
+}
+else if(jogos >= 20 && jogos >= 49){
+console.log(`Valor a pagar  : ${desconto30}`)
+}
+else if(jogos >= 50 && jogos >= 99){
+console.log(`Valor a pagar  : ${desconto40}`)
+}
+else if(jogos >= 100){
+console.log(`Valor a pagar  : ${desconto50}`)
 }
 
-const valorTotalSemDesconto = jogos * precoPorJogo;
-const valorTotalComDesconto = valorTotalSemDesconto * (1 - desconto);
-
-console.log("Valor total sem desconto: R$", valorTotalSemDesconto);
-console.log("Desconto de ",desconto * 100,"% aplicado: R$" ,valorTotalSemDesconto - valorTotalComDesconto)
-console.log("Valor total com desconto: R$", valorTotalComDesconto);
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-/*10. Uma loja de jogos vende cada jogo por R$ 150.00, mas concede descontos
-conforme a quantidade comprada, conforme a tabela a seguir:
-Quantidade de jogos comprados
-Desconto
-10–19
-20%
-20–49
-30%
-50 a 99
-40%
-100 ou mais
-50%
-Crie um algoritmo que leia o número de jogos comprados, o valor original da
-compra, o valor do desconto (se houver) e o valor total da compra após o desconto.
+/*10. Uma loja de jogos vende cada jogo por R$ 150.00, mas concede descontos 
+conforme a quantidade comprada, conforme a tabela a seguir: 
+Quantidade de jogos comprados 
+10–19 
+Desconto 
+20% 
+20–49 
+50 a 99 
+30% 
+40% 
+100 ou mais 
+50% 
+Crie um algoritmo que leia o número de jogos comprados, o valor original da 
+compra, o valor do desconto (se houver) e o valor total da compra após o desconto. 
 */
